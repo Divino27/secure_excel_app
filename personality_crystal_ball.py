032a832bba3ed2ee112bb78abff0d1d9c9aa8aa3
Divino27 <divyavinoth27012013@gmail.com>
2025-12-02 19:26:57 +0530

    Update personality_crystal_ball.py

--- personality_crystal_ball.py
+++ personality_crystal_ball.py
@@ -58,7 +58,7 @@ st.markdown(f"""
     <img src="{fun_image}" class="top-image">
     <h1 style="color:#8A2BE2; font-weight:900;">Welcome to the Computer Expo 2025 🎉</h1>
     <h2 style="color:#FF1493;">Amrita Vidyalayam, Kovur</h2>
-    <h4 style="color:#333;">A Creative Project by V. Madhavan & Siddarth, 7A 💻✨</h4>
+    <h4 style="color:#333;">A Creative Project by V. Madhavan, 7A 💻✨</h4>
     <img src="{robot_image}" class="robot-image">
 </div>
 """, unsafe_allow_html=True)
